afa48f955c54f4d05ad153a08a38e219366c2a4d
NiFoox <nifoox@gmail.com>
2025-11-07 23:16:16 -0300

    feat: add game card, form, create, detail, and edit components with templates and tests

---
+++ src/app/features/games/pages/game-edit/game-edit.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-game-edit',
+  imports: [],
+  templateUrl: './game-edit.html',
+  styleUrl: './game-edit.scss',
+})
+export class GameEdit {
+
+}
