4e676d9894ecd3646206b212d374636da0fa5d1f
NiFoox <nifoox@gmail.com>
2025-12-05 22:00:59 -0300

    refactor: clean up app configuration and toolbar components

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,4 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
-<<<<<<< HEAD
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
@@ -7,23 +6,12 @@ import { provideAnimationsAsync } from '@angular/platform-browser/animations/asy
 
 import { routes } from './app.routes';
 import { authInterceptor } from './core/auth/auth.interceptor';
-=======
-import { provideRouter, withComponentInputBinding } from '@angular/router';
-
-import { routes } from './app.routes';
-import { provideHttpClient, withFetch, withInterceptorsFromDi } from '@angular/common/http';
->>>>>>> develop
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
-<<<<<<< HEAD
     provideRouter(routes),
     provideHttpClient(withInterceptors([authInterceptor])),
     provideAnimationsAsync()
-=======
-    provideRouter(routes, withComponentInputBinding()),
-    provideHttpClient(withFetch(), withInterceptorsFromDi())
->>>>>>> develop
   ]
 };
